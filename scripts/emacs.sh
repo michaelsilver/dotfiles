cc303ba4e969136d8747d96e73d790985fa42563
Michael Silver <me@michaelsilver.io>
2021-03-21 22:11:11 -0400

    Add raycast scripts

---
+++ scripts/emacs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title emacs
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon emacs.png
+# @raycast.currentDirectoryPath ~
+# @raycast.packageName
+
+open -a /Applications/Emacs.app/
